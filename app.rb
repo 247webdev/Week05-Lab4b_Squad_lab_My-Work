0ac742c91605ff6ee23b9f0414b91eb5b47cfda7
Christian Chandler <247webdev+github@googlemail.com>
2015-02-15 16:51:58 -0800

    commit simply to backup. Functions still in progress

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'better_errors'
-require 'pry' # allows binding.pry  to give you breakpoints in code
+require 'pry'
 require 'pg'
 
 configure :development do
@@ -19,72 +19,89 @@ get '/' do
   redirect to '/squads'
 end
 
-# this route should take the user to a page that shows all of the squads
+# GET ROUTES
+# This takes the user to a page that shows all of the squads
 get '/squads' do
   squads = []
-  @conn.exec("SELECT * FROM squads") do |result|
+  @conn.exec("SELECT * FROM squads ORDER BY squad_id ASC") do |result|
       result.each do |author|
         squads << author
       end
   end
-  p "___________________________________________________________"
-  p "                     SQUADS                                "
-  p squads
-  p "                                                           "
-  p "___________________________________________________________"
   @squads = squads
   erb :index
 end
 
-# this route should take the user to a page with a form that allows them to create a new squad
+# This takes the user to a page with a form that allows them to create a new squad
 get '/squads/new' do
   erb :new
 end
 
-# this route should take the user to a page that shows information about a single squad
+# This takes the user to a page that shows information about a single squad
 get '/squads/:squad_id' do
+  squad_id = params[:squad_id].to_i
+  squad = @conn.exec("SELECT * FROM squads WHERE squad_id=$1", [squad_id])
+  @squad = squad[0]
   erb :show
 end
 
-# this route should take the user to a page with a form that allows them to edit an existing squad
+# This takes the user to a page with a form that allows them to edit an existing squad
 get '/squads/:squad_id/edit' do
+  squad_id = params[:squad_id].to_i
+  squad = @conn.exec("SELECT * FROM squads WHERE squad_id=$1", [squad_id])
+  @squad = squad[0]
+  students = @conn.exec("SELECT * FROM students WHERE squad_id=$1", [squad_id])
+  @student_count = students.length
+  @students = students
+  
+# Debugging output to terminal
+  p "___________________________________________________________"
+  p "                     squad                                "
+  p @squad
+  p "                     students                                "
+  p @students
+  p "                                                           "
+  p "___________________________________________________________"
+# END debugging
   erb :edit
 end
 
-# this route should take the user to a page that shows all of the students for an individual squad
+# This takes the user to a page that shows all of the students for an individual squad
 get '/squads/:squad_id/students' do
   redirect to '/squads'
 end
 
-# this route should take the user to a page that shows information about an individual student in a squad
+# This takes the user to a page that shows information about an individual student in a squad
 get '/squads/:squad_id/students/:student_id' do
   redirect to '/squads/:id'
 end
 
-# this route should take the user to a page that shows them a form to create a new student
+# This takes the user to a page that shows them a form to create a new student
 get '/squads/:squad_id/students/new' do
   redirect to '/squads/:id'
 end
 
-# this route should take the user to a page that shows them a form to edit a student's information
+# This takes the user to a page that shows them a form to edit a student's information
 get '/squads/:squad_id/students/:student_id/edit' do
   #-
 end
 
-# this route should be used for creating a new squad
+# POST ROUTES
+# This creates a new squad
 post '/squads' do
-  #logic to add squad
+  @conn.exec("INSERT INTO squads (name,mascot) VALUES ($1,$2)", [params[:name],params[:mascot]] )
 
   redirect to '/squads'
 end
 
-# this route should be used for creating a new student in an existing squad
+# This creates a new student in an existing squad
 post '/squads/:squad_id/students' do
   #logic to add a student that's in a squad
 
   redirect to '/squads'
 end
 
+# PUT ROUTES
 # this route should be used for editing an existing squad
 put '/squads/:squad_id' do
 
@@ -94,10 +111,11 @@ end
 put '/squads/:squad_id/students' do 
 end
 
+# DELETE ROUTES
 # this route should be used for deleting an existing squad
 delete '/squads/:squad_id' do
-  id = params[:id].to_i
-  @conn.exec("DELETE FROM squads WHERE id=$1", [id])
+  id = params[:squad_id].to_i
+  @conn.exec("DELETE FROM squads WHERE squad_id=$1", [id])
   redirect to '/squads'
 end
 
@@ -105,8 +123,37 @@ end
 delete '/squads/:squad_id/students/:student_id' do
 end
 
+# THIS ROUTE IS FOR DEMO PURPOSES ONLY
+# This adds a pre-set Squad list for a quick re-population
+# I know this normally would be defined as a post route by the link to this route is an <a>. Doing this way for convenience.
+get '/repopulate_squads' do
+  @conn.exec("INSERT INTO squads (name,mascot) VALUES ('TEST - Best Name Ever','My Little Pony')")
+  @conn.exec("INSERT INTO squads (name,mascot) VALUES ('TEST - Bad Ass','Three toed sloth')")
+  @conn.exec("INSERT INTO squads (name,mascot) VALUES ('TEST - Party of 5','Horny Toad')")
+  redirect to '/squads'
+end
+
+# This adds a pre-set Student list for a quick re-population
+# I know this normally would be defined as a post route by the link to this route is an <a>. Doing this way for convenience.
+get '/repopulate_students' do
+  @conn.exec("INSERT INTO students (squad_id,name,age,spirit_animal,is_squad_leader) VALUES (2,'TEST-Bob DropTable',99,'Flamingo',false)")
+  @conn.exec("INSERT INTO students (squad_id,name,age,spirit_animal,is_squad_leader) VALUES (3,'TEST-SueSelectAll',88,'DragonFly',false)")
+  @conn.exec("INSERT INTO students (squad_id,name,age,spirit_animal,is_squad_leader) VALUES (4,'TEST-IanInsert',77,'Dung Beatle',false)")
+  redirect to '/squads'
+end
+
 ### Bonus
 
 #1. Style your application!
 #2. Add a column to the students table called `is_squad_leader`, which is a `boolean`. When you list out the students in a squad, their name should be bolded.
 #2. Use your knowledge of JavaScript and AJAX to make the page more dynamic.
+
+# # Debugging output to terminal
+#   p "___________________________________________________________"
+#   p "                     SQUADS                                "
+#   p squads
+#   p "                                                           "
+#   p "___________________________________________________________"
+# # END debugging
+
+
--- public/app.js
+++ public/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  
+  $('.delete').on("click", function(){
+
+    $('.delete').css("position", "relative").css("top","2px").css("left","2px");
+
+  });
 });
